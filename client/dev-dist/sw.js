d6b3987a6a4a784cb53b42409d61d777f87cf4ff
Anuwat Thisuka <43104719+AnuwatThisuka@users.noreply.github.com>
2024-08-31 09:46:24 +0700

    feat: Update service worker and API service

--- client/dev-dist/sw.js
+++ client/dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-b5f7729d'], (function (workbox) { 'use strict';
     "revision": "3ef374a073038c176a8fefc88acb949f"
   }, {
     "url": "index.html",
-    "revision": "0.04mn076889o"
+    "revision": "0.tdff7nr298o"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
