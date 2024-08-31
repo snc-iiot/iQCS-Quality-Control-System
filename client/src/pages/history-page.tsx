d6df56d3bb5b3d8c4c5e062c1fd7d6b64943742b
Anuwat Thisuka <43104719+AnuwatThisuka@users.noreply.github.com>
2024-08-31 15:39:02 +0700

    chore: Update dependencies, refactor validation schemas, update API service, and fix part page table layout

--- client/src/pages/history-page.tsx
+++ client/src/pages/history-page.tsx
@@ -1,5 +1,4 @@
 import { DefectDetail } from "@/components/common/defect-detail";
-// import { PageHeader } from "@/components/common/page-header";
 import { CreateUpdateDefect, CreateUpdateProductivity } from "@/components/form";
 import { DateInputForm } from "@/components/ui-pattern/form-field/input-form";
 import { SelectForm } from "@/components/ui-pattern/form-field/select-form";
