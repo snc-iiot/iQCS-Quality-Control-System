ddae9a74f52553438516f069a42845149f85b0fa
sataporn1995 <satapornchai2538@gmail.com>
2024-09-02 16:55:07 +0700

    Created [POST] /parts/import-excel

--- server/src/services/parts/parts.service.ts
+++ server/src/services/parts/parts.service.ts
@@ -94,46 +94,28 @@ export class PartsService {
 
       const partCodeExists = allPartCode.map((part) => part.part_code);
 
-      /*
-      //! Block duplicate part code
-      const partExists = await this.partRepository.find({
-        where: { part_code: input.part_code, plant_code: decoded.plant_code },
-        take: 1,
-      });
+      const newPartCode = input.data.filter(
+        (part) => !partCodeExists.includes(part.part_code),
+      );
 
-      if (partExists.length > 0)
-        return {
-          status: 'error',
-          statusCode: 400,
-          message: 'Part already exists',
-          data: [],
-        };
-
-      const record = {
-        sap_code: input.sap_code,
-        part_code: input.part_code,
-        part_name: input.part_name,
-        part_description: input.part_description ?? '',
-        processes: input.processes,
-        price: input.price,
-        customers: input.customers ?? [],
+      const records = newPartCode.map((part) => ({
+        sap_code: part.sap_code ?? null,
+        part_code: part.part_code,
+        part_name: part.part_name,
+        part_description: part.part_description ?? '',
+        processes: part.processes,
+        price: part.price ?? 0,
+        customers: part.customers ?? [],
         plant_code: decoded.plant_code,
-      };
+      }));
 
-      // return {
-      //   status: 'success',
-      //   statusCode: 200,
-      //   message: 'Demo1',
-      //   data: [{ record }],
-      // };
-      const created = await this.partRepository.save(record);
-      */
+      const created = await this.partRepository.save(records);
 
       return {
         status: 'success',
         statusCode: 200,
-        message: 'Part created successfully',
-        data: [input, decoded],
+        message: 'Parts created successfully',
+        data: [created],
       };
     } catch (error) {
       return {
