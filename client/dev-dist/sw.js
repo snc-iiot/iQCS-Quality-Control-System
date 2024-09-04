79d94500aa91c0a9a8c4bd02e2666a32205b9665
Anucha3666 <Anucha.ram3666@gmail.com>
2024-09-04 16:21:59 +0700

    Update document

--- client/dev-dist/sw.js
+++ client/dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-b5f7729d'], (function (workbox) { 'use strict';
     "revision": "3ef374a073038c176a8fefc88acb949f"
   }, {
     "url": "index.html",
-    "revision": "0.incoo49nodo"
+    "revision": "0.sq0jgrss54g"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
