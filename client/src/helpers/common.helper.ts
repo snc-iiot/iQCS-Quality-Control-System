9d33d5d5c90e379ad107d0a1ad5448aac63a38f4
Anucha3666 <Anucha.ram3666@gmail.com>
2024-11-16 16:49:41 +0700

    Update API_BASE_URL

--- client/src/helpers/common.helper.ts
+++ client/src/helpers/common.helper.ts
@@ -1,7 +1,7 @@
 // export const API_BASE_URL = "http://10.1.8.175:3000/api";
 
-// export const API_BASE_URL = "https://api-gateway-v1.sncformer.com/iqcs/prod/v1";
-export const API_BASE_URL = "http://10.1.9.69:3000/api";
+export const API_BASE_URL = "https://api-gateway-v1.sncformer.com/iqcs/prod/v1";
+// export const API_BASE_URL = "http://10.1.9.69:3000/api";
 
 // export const API_BASE_URL = "https://api-gateway-v1.sncformer.com/iqcs/dev/v1";
 
